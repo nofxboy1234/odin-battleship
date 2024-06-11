0b3216272d3a782783192e976e56fbbb19dc58c1
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-11 16:25:02 +0200

    Add New Game button and add event listener to it

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import Gameboard from './gameboard';
 import Player from './player';
 import Ship from './ship';
 
+const human = new Player();
+const computer = new Player();
+
 console.log('hello!');
+
+const newGameButton = document.getElementById('new-game-btn');
+newGameButton.addEventListener('click', () => {
+  console.log('New game!');
+});
