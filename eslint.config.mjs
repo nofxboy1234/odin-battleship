da1c350251e993c011d8c5d59a1bd8a712f63382
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-11 09:45:11 +0200

    Add ...globals.node to eslint config to fix undef errors for `module` and `require`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,11 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.jest } } },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.jest, ...globals.node },
+    },
+  },
   pluginJs.configs.recommended,
   {
     ignores: ['**/node_modules/', '**/dist/', 'package-lock.json'],
