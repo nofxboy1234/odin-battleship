c61786ba89efec9ad3d7f48c4a8b148a44614967
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-11 16:01:21 +0200

    Add launch config for dist/index.html and import battleship modules into index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
-import myName from './myName';
-import { functionOne, functionTwo } from './myModule';
 import './style.css';
-import Icon from './icon.png';
+import Gameboard from './gameboard';
+import Player from './player';
+import Ship from './ship';
 
-import printMe from './print.js';
-
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  // Lodash, now imported by this script
-  element.innerHTML = 'Hello webpack';
-  element.classList.add('hello');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  element.appendChild(btn);
-
-  console.log(myName('Dylan'));
-  console.log(functionOne());
-  console.log(functionTwo());
-
-  return element;
-}
-
-document.body.appendChild(component());
+console.log('hello!');
