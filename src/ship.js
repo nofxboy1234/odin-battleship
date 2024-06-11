d5d741d18b7f6324f2233b8ca9614d299b5e157e
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-11 09:40:54 +0200

    Update sum and sum.test to use CommonJS modules
    Check to see that both ES6 and CommonJS modules work with jest

---
+++ src/ship.js
@@ -0,0 +1,17 @@
+// class Ship {
+//   #secret = 1234;
+
+//   constructor() {
+//     this.length = 4;
+//     this.hits = 0;
+//   }
+
+//   showSecret() {
+//     console.log(this.#secret);
+//   }
+// }
+
+// const ship1 = new Ship();
+// ship1.showSecret();
+
+// console.log('end');
---
+++ tests/ship.test.js
@@ -0,0 +1,3 @@
+// test('hit() increases the number of hits', () => {
+//   expect(ship1.hit());
+// })
