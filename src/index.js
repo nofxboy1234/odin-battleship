571775d8a3ba4ec11408e929e2a2bcff9b92e1ac
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-11 17:18:01 +0200

    Refactor code into placePlayerShips()

--- src/index.js
+++ src/index.js
@@ -3,24 +3,30 @@ import Gameboard from './gameboard';
 import Player from './player';
 import Ship from './ship';
 
+function placePlayerShips(player) {
+  player.gameboard.placeShip(new Ship(4), 0, 0);
+
+  player.gameboard.placeShip(new Ship(3), 0, 2);
+  player.gameboard.placeShip(new Ship(3), 4, 2);
+
+  player.gameboard.placeShip(new Ship(2), 0, 4);
+  player.gameboard.placeShip(new Ship(2), 3, 4);
+  player.gameboard.placeShip(new Ship(2), 6, 4);
+
+  player.gameboard.placeShip(new Ship(1), 0, 6);
+  player.gameboard.placeShip(new Ship(1), 2, 6);
+  player.gameboard.placeShip(new Ship(1), 4, 6);
+  player.gameboard.placeShip(new Ship(1), 6, 6);
+}
+
 function newGame(event) {
   console.log('New game!');
 
   const human = new Player();
+  placePlayerShips(human);
 
-  human.gameboard.placeShip(new Ship(4), 0, 0);
-
-  human.gameboard.placeShip(new Ship(3), 0, 2);
-  human.gameboard.placeShip(new Ship(3), 4, 2);
-
-  human.gameboard.placeShip(new Ship(2), 0, 4);
-  human.gameboard.placeShip(new Ship(2), 3, 4);
-  human.gameboard.placeShip(new Ship(2), 6, 4);
-
-  human.gameboard.placeShip(new Ship(1), 0, 6);
-  human.gameboard.placeShip(new Ship(1), 2, 6);
-  human.gameboard.placeShip(new Ship(1), 4, 6);
-  human.gameboard.placeShip(new Ship(1), 6, 6);
+  const computer = new Player();
+  placePlayerShips(computer);
 
   // const computer = new Player();
 }
