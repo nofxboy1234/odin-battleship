4b9570f746f886d9b5bfbca98760ab2822d8e3f2
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-12 13:13:39 +0200

    Refactor renderGameboard()

--- src/domHelper.js
+++ src/domHelper.js
@@ -15,17 +15,17 @@ function renderGameboard(gameboard, targetID) {
   for (let index = 0; index < 121; index++) {
     const div = document.createElement('div');
 
-    // div.textContent = index;
-
     const columns = 'ABCDEFGHIJ';
     const rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    if (index >= 1 && index <= 10) {
+    if (index === 0) {
+      div.classList.add('cell-label');
+    } else if (index >= 1 && index <= 10) {
+      div.classList.add('cell-label');
       div.textContent = columns[index - 1];
-      div.classList.add('cell-label');
     } else if (index >= 11 && index % 11 === 0) {
-      div.textContent = rows.at((index % 10) - 1);
       div.classList.add('cell-label');
+      div.textContent = rows.at((index % 10) - 1);
     } else {
       div.classList.add('cell');
     }
