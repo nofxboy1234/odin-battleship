f7fb96b5770aed5860469fab66e80213a1b53df3
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-13 18:02:14 +0200

    Exclude labels from gameBoard when rendering

--- src/domHelper.js
+++ src/domHelper.js
@@ -4,33 +4,6 @@ function renderShips(ships, targetID) {
   });
 }
 
-function isCellTopLeftBlank(index) {
-  return index === 0;
-}
-
-function isCellLabelTop(index) {
-  return index >= 1 && index <= 10;
-}
-
-function isCellLabelLeft(index) {
-  return index >= 11 && index % 11 === 0;
-}
-
-function zeroBasedIndex(index) {
-  const base10 = index % 10;
-
-  if (index % 10 === 0) {
-    return index / 10 - 2;
-  } else {
-    if (index % 11 === 0) {
-      return (index % 10) - 2;
-    }
-    return (index % 10) - 2;
-  }
-
-  // return index % 10 === 0 ? index / 10 - 2 : (index % 10) - 2;
-}
-
 function renderGameboardCellNumbers(targetID) {
   const container = document.getElementById(targetID);
   const gameboardDiv = document.createElement('div');
@@ -45,36 +18,57 @@ function renderGameboardCellNumbers(targetID) {
   }
 }
 
+function zipArrays(a, b) {
+  const c = a.map(function (e, i) {
+    return [e, b[i]];
+  });
+
+  return c;
+}
+
 function renderGameboard(targetID) {
   const container = document.getElementById(targetID);
-  const columns = 'ABCDEFGHIJ';
 
   const gameboardDiv = document.createElement('div');
   gameboardDiv.classList.add('gameboard');
   container.appendChild(gameboardDiv);
 
-  for (let index = 0; index < 121; index++) {
+  const xArray = [];
+  for (let value = 0; value < 10; value++) {
+    for (let index = 0; index < 10; index++) {
+      xArray.push(index);
+    }
+  }
+
+  const yArray = [];
+
+  for (let value = 0; value < 10; value++) {
+    for (let index = 0; index < 10; index++) {
+      yArray.push(value);
+    }
+  }
+
+  const xyArray = zipArrays(xArray, yArray);
+
+  for (let index = 0; index < 100; index++) {
     const div = document.createElement('div');
 
-    if (isCellTopLeftBlank(index)) {
-      div.classList.add('cell-label');
-    } else if (isCellLabelTop(index)) {
-      div.classList.add('cell-label', 'bottom-border');
-      div.textContent = columns[index - 1];
-    } else if (isCellLabelLeft(index)) {
-      div.classList.add('cell-label', 'right-border');
-      div.textContent = index % 10 === 0 ? index / 11 : index % 10;
-    } else {
-      div.classList.add('cell');
+    div.classList.add('cell');
 
-      const x = (index - 12) % 11;
-      const y = (index - 11) % 10;
-      // const y = index - 12;
-      div.textContent = `${x}, ${y}`;
-      // div.textContent = index - 12;
+    const x = xyArray[index][0];
+    const y = xyArray[index][1];
+    div.textContent = `${x}, ${y}`;
 
-      // div.textContent = zeroBasedIndex(index);
-    }
+    // if (isCellTopLeftBlank(index)) {
+    //   div.classList.add('cell-label');
+    // } else if (isCellLabelTop(index)) {
+    //   div.classList.add('cell-label', 'bottom-border');
+    //   div.textContent = columns[index - 1];
+    // } else if (isCellLabelLeft(index)) {
+    //   div.classList.add('cell-label', 'right-border');
+    //   div.textContent = index % 10 === 0 ? index / 11 : index % 10;
+    // } else {
+    // }
 
     gameboardDiv.appendChild(div);
   }
