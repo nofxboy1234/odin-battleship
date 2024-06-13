d893a2af4d5e92c1b6f9277cf9e21c3ebb536f63
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-13 15:05:05 +0200

    Refactor renderGameboard() to show 0-based `x, y` co-ords in each cell

--- src/domHelper.js
+++ src/domHelper.js
@@ -16,6 +16,21 @@ function isCellLabelLeft(index) {
   return index >= 11 && index % 11 === 0;
 }
 
+function zeroBasedIndex(index) {
+  const base10 = index % 10;
+
+  if (index % 10 === 0) {
+    return index / 10 - 2;
+  } else {
+    if (index % 11 === 0) {
+      return (index % 10) - 2;
+    }
+    return (index % 10) - 2;
+  }
+
+  // return index % 10 === 0 ? index / 10 - 2 : (index % 10) - 2;
+}
+
 function renderGameboardCellNumbers(targetID) {
   const container = document.getElementById(targetID);
   const gameboardDiv = document.createElement('div');
@@ -51,6 +66,13 @@ function renderGameboard(targetID) {
       div.textContent = index % 10 === 0 ? index / 11 : index % 10;
     } else {
       div.classList.add('cell');
+
+      const x = (index - 12) % 11;
+      const y = (index - 12) % 11;
+      div.textContent = `${x}, ${y}`;
+      // div.textContent = index - 12;
+
+      // div.textContent = zeroBasedIndex(index);
     }
 
     gameboardDiv.appendChild(div);
