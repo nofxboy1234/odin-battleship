783a2a9cb8bdae44651482b12949d8ba752c2e74
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-15 07:59:09 +0200

    Refactor

--- src/gameboard.js
+++ src/gameboard.js
@@ -11,7 +11,7 @@ class Gameboard {
   }
 
   receiveAttack(x, y) {
-    const hitShip = this.#wasShipHit(x, y);
+    const hitShip = this.#getShipOnCell(x, y);
     if (hitShip) {
       hitShip.hit(x, y);
     } else {
@@ -30,8 +30,8 @@ class Gameboard {
     return [...this.#ships];
   }
 
-  isShipOnCell(x, y) {
-    return !!this.#ships.find((ship) => {
+  #getShipOnCell(x, y) {
+    return this.#ships.find((ship) => {
       const shipBack = ship.x;
       const shipFront = ship.x + ship.length;
 
@@ -39,24 +39,8 @@ class Gameboard {
     });
   }
 
-  #wasShipHit(x, y) {
-    let hitShip;
-    this.#ships.forEach((ship) => {
-      if (y === ship.y) {
-        if (this.#shotHitsHorizontalLength(x, ship)) {
-          hitShip = ship;
-        }
-      }
-    });
-
-    return hitShip;
-  }
-
-  #shotHitsHorizontalLength(x, ship) {
-    const shipBack = ship.x;
-    const shipFront = ship.x + (ship.length - 1);
-
-    return x >= shipBack && x <= shipFront;
+  isShipOnCell(x, y) {
+    return !!this.#getShipOnCell(x, y);
   }
 
   #isNewShot(x, y) {
