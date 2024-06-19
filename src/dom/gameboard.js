d0a6b09a0ca13a158e26892f76759018ff7f18f3
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-19 11:21:59 +0200

    Refactor

--- src/dom/gameboard.js
+++ src/dom/gameboard.js
@@ -15,10 +15,10 @@ class Gameboard {
     const yArray = createYArray();
     const xyArray = zipArrays(xArray, yArray);
 
-    this.createCellLabelsLeft();
-    this.createLabelsLeftCells();
-    this.createCellLabelsTop();
-    this.createLabelsTopCells();
+    this.labelsLeftContainer = this.createLabelsLeftContainer();
+    this.createLabelsLeft();
+    this.labelsTopContainer = this.createLabelsTopContainer();
+    this.createLabelsTop();
 
     this.createCells(xyArray, controller);
     this.createShips(controller);
@@ -49,56 +49,49 @@ class Gameboard {
     return this.#disabled;
   }
 
-  createCellLabelsLeft() {
-    const containers = document.getElementsByClassName(
-      'cell-labels-left-container',
-    );
-    [...containers].forEach((parent) => {
-      const div = document.createElement('div');
-      div.classList.add('cell-labels-left');
-      parent.appendChild(div);
-    });
+  createLabelsLeftContainer() {
+    const element = document.createElement('div');
+    element.classList.add('gameboard-0');
+    this.appendChild(element);
+
+    return element;
   }
 
-  createLabelsLeftCells() {
-    const cellLabelElementsLeft =
-      document.getElementsByClassName('cell-labels-left');
+  createLabelsLeft() {
     const labels = ['', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 
-    [...cellLabelElementsLeft].forEach((element) => {
-      for (let index = 0; index < labels.length; index++) {
-        const div = document.createElement('div');
+    labels.forEach((label, index) => {
+      const element = document.createElement('div');
 
-        if (index === 0) {
-          div.classList.add('cell-label');
-        } else {
-          div.classList.add('cell-label', 'right-border');
-        }
-
-        div.textContent = labels[index];
-        element.appendChild(div);
+      if (index === 0) {
+        element.classList.add('gameboard-0-blank');
+      } else {
+        element.classList.add('gameboard-0-label');
       }
+
+      element.textContent = label;
+      this.labelsLeftContainer.appendChild(element);
     });
   }
 
-  createCellLabelsTop() {
-    const div = document.createElement('div');
-    div.classList.add('cell-labels-top');
-    this.appendChild(div);
+  createLabelsTopContainer() {
+    const element = document.createElement('div');
+    element.classList.add('gameboard-1-0');
+    this.appendChild(element);
+
+    return element;
   }
 
-  createLabelsTopCells() {
-    const cellLabelElementsTop =
-      document.getElementsByClassName('cell-labels-top');
+  createLabelsTop() {
     const labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
-    [...cellLabelElementsTop].forEach((element) => {
-      for (let index = 0; index < labels.length; index++) {
-        const div = document.createElement('div');
-        div.classList.add('cell-label', 'bottom-border');
-        div.textContent = labels[index];
-        element.appendChild(div);
-      }
+    labels.forEach((label, index) => {
+      const element = document.createElement('div');
+
+      element.classList.add('gameboard-1-0-label');
+
+      element.textContent = label;
+      this.labelsTopContainer.appendChild(element);
     });
   }
 
