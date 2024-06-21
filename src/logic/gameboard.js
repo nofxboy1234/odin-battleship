fb787b44e4082d5854cac822fbbed60e8563ad33
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-21 14:08:20 +0200

    Add #createCellArray() to logic/gameboard

--- src/logic/gameboard.js
+++ src/logic/gameboard.js
@@ -51,6 +51,17 @@ class Gameboard {
     return !!this.#getHitOnCell(x, y) || !!this.#getMissOnCell(x, y);
   }
 
+  #createCellArray() {
+    const cellArray = [];
+
+    for (let y = 0; y < 10; y++) {
+      for (let x = 0; x < 10; x++) {
+        cellArray.push([x, y]);
+      }
+    }
+    return cellArray;
+  }
+
   #getHitOnCell(x, y) {
     const hits = this.getHits();
     return hits.find(([hitX, hitY]) => x === hitX && y === hitY);
