3cedb2314f71f695adb540e85da2f4edb29de927
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-21 14:40:00 +0200

    Add test for player.play()

--- tests/player.test.js
+++ tests/player.test.js
@@ -1,5 +1,6 @@
 import Gameboard from '../src/logic/gameboard';
 import Player from '../src/logic/player';
+import Ship from '../src/logic/ship';
 
 test('player contains a gameboard', () => {
   const gameboard = new Gameboard();
@@ -12,6 +13,18 @@ test('play() returns an array of length 2', () => {
   const gameboard = new Gameboard();
   const player = new Player(gameboard);
   const enemyGameboard = new Gameboard();
+
+  expect(player.play(enemyGameboard).length).toBe(2);
+});
+
+test('play() returns a new shot', () => {
+  const gameboard = new Gameboard();
+  const player = new Player(gameboard);
+  const enemyGameboard = new Gameboard();
+  const ship = new Ship(3);
+  enemyGameboard.placeShip(ship, 0, 0);
+  enemyGameboard.receiveAttack(0, 0);
+  enemyGameboard.receiveAttack(5, 5);
 
   expect(player.play(enemyGameboard).length).toBe(2);
 });
