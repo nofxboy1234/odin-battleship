d501f8c7dd1a79d6c19db84ab85a940728cc373b
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-24 10:18:23 +0200

    Add tests for player.play() with different Gameboard states

--- tests/player.test.js
+++ tests/player.test.js
@@ -17,7 +17,7 @@ test('play() returns an array of length 2', () => {
   expect(player.play(enemyGameboard).length).toBe(2);
 });
 
-test('play() returns a new shot', () => {
+test('play() returns a new random shot from an available array of 1 after 1 hit and 2 misses', () => {
   const gameboard = new Gameboard(2);
   const player = new Player(gameboard);
   const enemyGameboard = new Gameboard(2);
@@ -33,4 +33,36 @@ test('play() returns a new shot', () => {
     [1, 0],
     [0, 1],
   ]).not.toContainEqual(player.play(enemyGameboard));
+});
+
+test('play() returns a new random shot from an available array of 3 after 1 hit', () => {
+  const gameboard = new Gameboard(2);
+  const player = new Player(gameboard);
+  const enemyGameboard = new Gameboard(2);
+  const ship = new Ship(1);
+  enemyGameboard.placeShip(ship, 0, 0);
+  enemyGameboard.receiveAttack(0, 0);
+
+  expect([
+    [1, 0],
+    [0, 1],
+    [1, 1],
+  ]).toContainEqual(player.play(enemyGameboard));
+  expect([[0, 0]]).not.toContainEqual(player.play(enemyGameboard));
+});
+
+test('play() returns a new random shot from an available array of 3 after 1 miss', () => {
+  const gameboard = new Gameboard(2);
+  const player = new Player(gameboard);
+  const enemyGameboard = new Gameboard(2);
+  const ship = new Ship(1);
+  enemyGameboard.placeShip(ship, 0, 0);
+  enemyGameboard.receiveAttack(0, 1);
+
+  expect([
+    [0, 0],
+    [1, 0],
+    [1, 1],
+  ]).toContainEqual(player.play(enemyGameboard));
+  expect([[0, 1]]).not.toContainEqual(player.play(enemyGameboard));
 });
