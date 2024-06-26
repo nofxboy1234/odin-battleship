9cc69638a6157c36c510a340a73077d2621f273a
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-26 15:43:12 +0200

    Add test for hasOverlappingShips()

--- tests/rules.test.js
+++ tests/rules.test.js
@@ -68,3 +68,62 @@ describe('When 2 ships have the same cells', () => {
     expect(hasOverlappingShips(ships)).toEqual(true);
   });
 });
+
+describe('When every cell of every ships is different to every cell of all other ships', () => {
+  test('hasOverlappingShips() returns false', () => {
+    const gameboard = new Gameboard();
+    const ships = [];
+
+    const battleship1 = new Battleship();
+    battleship1.setHorizontal();
+    battleship1.place(0, 0, gameboard);
+    ships.push(battleship1);
+
+    const destroyer1 = new Destroyer();
+    destroyer1.setHorizontal();
+    destroyer1.place(0, 1, gameboard);
+    ships.push(destroyer1);
+
+    const destroyer2 = new Destroyer();
+    destroyer2.setHorizontal();
+    destroyer2.place(0, 2, gameboard);
+    ships.push(destroyer2);
+
+    const submarine1 = new Submarine();
+    submarine1.setHorizontal();
+    submarine1.place(0, 3, gameboard);
+    ships.push(submarine1);
+
+    const submarine2 = new Submarine();
+    submarine2.setHorizontal();
+    submarine2.place(0, 4, gameboard);
+    ships.push(submarine2);
+
+    const submarine3 = new Submarine();
+    submarine3.setHorizontal();
+    submarine3.place(0, 5, gameboard);
+    ships.push(submarine3);
+
+    const patrolboat1 = new PatrolBoat();
+    patrolboat1.setHorizontal();
+    patrolboat1.place(0, 6, gameboard);
+    ships.push(patrolboat1);
+
+    const patrolboat2 = new PatrolBoat();
+    patrolboat2.setHorizontal();
+    patrolboat2.place(0, 7, gameboard);
+    ships.push(patrolboat2);
+
+    const patrolboat3 = new PatrolBoat();
+    patrolboat3.setHorizontal();
+    patrolboat3.place(0, 8, gameboard);
+    ships.push(patrolboat3);
+
+    const patrolboat4 = new PatrolBoat();
+    patrolboat4.setHorizontal();
+    patrolboat4.place(0, 9, gameboard);
+    ships.push(patrolboat4);
+
+    expect(hasOverlappingShips(ships)).toEqual(false);
+  });
+});
