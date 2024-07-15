9e24a49155a51d6d8975a3fed7a674c155121437
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-15 10:15:50 +0200

    Remove unused methods

--- src/logic/cell.js
+++ src/logic/cell.js
@@ -7,22 +7,6 @@ class Cell {
   offset(x, y) {
     return new Cell(this.x + x, this.y + y);
   }
-
-  isAgainstTopWall() {
-    return this.y === 0;
-  }
-
-  isAgainstRightWall(gameboard) {
-    return this.x === gameboard.size - 1;
-  }
-
-  isAgainstBottomWall(gameboard) {
-    return this.y === gameboard.size - 1;
-  }
-
-  isAgainstLeftWall() {
-    return this.x === 0;
-  }
 }
 
 export default Cell;
--- tests/cell.test.js
+++ tests/cell.test.js
@@ -1,5 +1,4 @@
 import Cell from '../src/logic/cell';
-import Gameboard from '../src/logic/gameboard';
 
 test('offset() returns a new Cell at the specified positive x and y offset', () => {
   const cell = new Cell(3, 4);
@@ -13,73 +12,4 @@ test('offset() returns a new Cell at the specified negative x and y offset', () 
   const offsetCell = new Cell(6, 1);
 
   expect(cell.offset(-1, -8)).toEqual(offsetCell);
-});
-
-describe('isAgainstTopWall()', () => {
-  describe('when a cell is against the top wall of a gameboard', () => {
-    test('it returns true', () => {
-      const cell = new Cell(6, 0);
-      expect(cell.isAgainstTopWall()).toBe(true);
-    });
-  });
-
-  describe('when a cell is not against the top wall of a gameboard', () => {
-    test('it returns false', () => {
-      const cell = new Cell(6, 1);
-      expect(cell.isAgainstTopWall()).toBe(false);
-    });
-  });
-});
-
-describe('isAgainstRightWall()', () => {
-  describe('when a cell is against the right wall of a gameboard', () => {
-    test('it returns true', () => {
-      const cell = new Cell(9, 1);
-      const gameboard = new Gameboard();
-      expect(cell.isAgainstRightWall(gameboard)).toBe(true);
-    });
-  });
-
-  describe('when a cell is not against the right wall of a gameboard', () => {
-    test('it returns false', () => {
-      const cell = new Cell(8, 1);
-      const gameboard = new Gameboard();
-      expect(cell.isAgainstRightWall(gameboard)).toBe(false);
-    });
-  });
-});
-
-describe('isAgainstBottomWall()', () => {
-  describe('when a cell is against the bottom wall of a gameboard', () => {
-    test('it returns true', () => {
-      const cell = new Cell(3, 9);
-      const gameboard = new Gameboard();
-      expect(cell.isAgainstBottomWall(gameboard)).toBe(true);
-    });
-  });
-
-  describe('when a cell is not against the bottom wall of a gameboard', () => {
-    test('it returns false', () => {
-      const cell = new Cell(3, 8);
-      const gameboard = new Gameboard();
-      expect(cell.isAgainstBottomWall(gameboard)).toBe(false);
-    });
-  });
-});
-
-describe('isAgainstLeftWall()', () => {
-  describe('when a cell is against the left wall of a gameboard', () => {
-    test('it returns true', () => {
-      const cell = new Cell(0, 3);
-      const gameboard = new Gameboard();
-      expect(cell.isAgainstLeftWall(gameboard)).toBe(true);
-    });
-  });
-
-  describe('when a cell is not against the left wall of a gameboard', () => {
-    test('it returns false', () => {
-      const cell = new Cell(1, 3);
-      expect(cell.isAgainstLeftWall()).toBe(false);
-    });
-  });
 });
