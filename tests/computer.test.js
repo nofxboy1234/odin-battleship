0143975009699def03590a6921ff994d17d9a6ef
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-15 16:44:55 +0200

    Add test for bug in computer.getNextShot()

--- tests/computer.test.js
+++ tests/computer.test.js
@@ -14,6 +14,109 @@ afterEach(() => {
 });
 
 describe('When there are 10 ships on the human gameboard', () => {
+  describe('when the ships are positioned according to a bug found', () => {
+    describe('when there are 4 misses on the board', () => {
+      let computerGameboard;
+      let computer;
+      let humanGameboard;
+      let human;
+
+      let ships;
+      let ship1;
+      let ship2;
+      let ship3;
+      let ship4;
+      let ship5;
+      let ship6;
+      let ship7;
+      let ship8;
+      let ship9;
+      let ship10;
+
+      beforeEach(() => {
+        computerGameboard = new Gameboard();
+        computer = new Computer(computerGameboard, 'computer');
+        humanGameboard = new Gameboard();
+        human = new Player(humanGameboard, 'human');
+        ships = [];
+
+        ship1 = new Battleship();
+        ship1.setHorizontal();
+        ship1.place(5, 0, humanGameboard);
+        ships.push(ship1);
+
+        ship2 = new Destroyer();
+        ship2.setHorizontal();
+        ship2.place(1, 1, humanGameboard);
+        ships.push(ship2);
+
+        ship3 = new Destroyer();
+        ship3.setHorizontal();
+        ship3.place(0, 4, humanGameboard);
+        ships.push(ship3);
+
+        ship4 = new Submarine();
+        ship4.setVertical();
+        ship4.place(2, 8, humanGameboard);
+        ships.push(ship4);
+
+        ship5 = new Submarine();
+        ship5.setVertical();
+        ship5.place(4, 8, humanGameboard);
+        ships.push(ship5);
+
+        ship6 = new Submarine();
+        ship6.setHorizontal();
+        ship6.place(8, 6, humanGameboard);
+        ships.push(ship6);
+
+        ship7 = new PatrolBoat();
+        ship7.setHorizontal();
+        ship7.place(4, 5, humanGameboard);
+        ships.push(ship7);
+
+        ship8 = new PatrolBoat();
+        ship8.setHorizontal();
+        ship8.place(5, 3, humanGameboard);
+        ships.push(ship8);
+
+        ship9 = new PatrolBoat();
+        ship9.setHorizontal();
+        ship9.place(6, 7, humanGameboard);
+        ships.push(ship9);
+
+        ship10 = new PatrolBoat();
+        ship10.setHorizontal();
+        ship10.place(9, 9, humanGameboard);
+        ships.push(ship10);
+
+        human.placeShips(ships);
+      });
+
+      test('it returns a random Cell for the next shot that is available', async () => {
+        computer.resetTarget();
+
+        humanGameboard.receiveAttack(5, 4);
+        humanGameboard.receiveAttack(6, 3);
+        humanGameboard.receiveAttack(6, 5);
+        humanGameboard.receiveAttack(7, 4);
+
+        await expect(
+          computer.getNextShot(humanGameboard),
+        ).resolves.toBeInstanceOf(Cell);
+      });
+
+      test('it does not call ship.potentialHits()', async () => {
+        const spy = jest.spyOn(Ship.prototype, 'potentialHits');
+
+        await computer.getNextShot(humanGameboard);
+        expect(spy).not.toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('When there are 10 ships on the human gameboard', () => {
   let computerGameboard;
   let computer;
   let humanGameboard;
